43a8287a5be8ceeaa64c36209dd66444eca6a2be
KLAW4life <kerene789@gmail.com>
2021-11-28 22:07:55 -0500

    Remove unnecessary code files

--- cart.js
+++
@@ -1,6 +0,0 @@
-let cart = document.querySelectorAll(".button is-link is-outlined py-4 is-medium is-fullwidth")
-
-for(let i=0; i<cart.length; i++)
-{
-    cart[i].addEventListener('click',() =>{console.log("added to cart")})
-}
