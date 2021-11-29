273ba935229ab135ab7e5c1efa59d815b32b6d53
Techgirlllll <71361191+Techgirlllll@users.noreply.github.com>
2021-11-28 21:54:49 -0500

    Testimonial, FAQs and HomePage

---
+++ Homepage, Testimonials and FAQs/faq.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="faq.css">
+    <?php include('head.php') ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FREQUENTLY ASKED QUESTIONS</title>
+</head>
+<body>
+    <div class="container">
+        <p class="heading">Frequently Asked Questions</p>
+        <img src="./images/Dias Designs Transparent Background.png" class='image'>
+        <div class="faqs">
+           <details>
+              <summary>Where are you located?</summary>
+              <p class="text"> Dias Designs is strictly an onlined-based business.</p>
+           </details>
+           <details>
+              <summary>How long do I have to wait to receive a customised product?</summary>
+              <p class="text"> A custom-made item usually takes 3-5 days to be completed. However, it may take a few more days depending on the design, colour and size of the item. </p>
+           </details>
+           <details>
+              <summary>Do you take measurements?</summary>
+              <p class="text">No, most syles require a bra size(for eg. 32B) and a hip measurement/accurate pants size.</p>
+           </details>
+           <details>
+              <summary>Do you do deliveries?</summary>
+              <p class="text"> Yes we do. We deliver to Spanish Town, Half-way-Tree and Portmore</p>
+           </details>
+           <details>
+              <summary>What is the cost for delivery?</summary>
+              <p class="text">The cost for delivery depends on the location, however; the price ranges from $500-$1000.</p>
+           </details>
+        </div>
+     </div>
+</body>
+</html>
---
+++ Homepage, Testimonials and FAQs/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="styles.css">
+    <?php include('head.php') ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <title> Dias Designs</title>
+</head>
+<body>
+    <section class = "main">
+        <div class = "information">
+            <div class= "text">
+                <h2>DIAS <br><span>DESIGNS</span></h2>
+                <p>Welcome to Dias Designs, where we provide self-made clothing items made from crochet.</p>
+                <button> SHOP NOW </button>
+            </div>
+            <div class= "main-img">
+                <div class = "img active">
+                    <img src="./images/Group 3(2).png">
+                </div>
+            </div>
+        </div>
+        <div class="media-icons">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+          </div>
+    </section>
+</body>
+</html>
